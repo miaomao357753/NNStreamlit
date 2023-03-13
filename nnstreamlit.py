75229a3b935572efc8e142ceda2d0f26d0654dc7
miaomao357753 <92762634+miaomao357753@users.noreply.github.com>
2023-03-13 19:42:14 -0400

    Update nnstreamlit.py

--- nnstreamlit.py
+++ nnstreamlit.py
@@ -6,8 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import precision_score, recall_score
+
 
 import time
 import numpy as np
